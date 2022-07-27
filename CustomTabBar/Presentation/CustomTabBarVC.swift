de1444c4bc764f336bfb01d9e4b196b495654572
Hishara Dilshan <hisharadilshan3@gmail.com>
2022-07-27 10:51:59 +0530

    Initial Commit

---
+++ CustomTabBar/Presentation/CustomTabBarVC.swift
@@ -0,0 +1,15 @@
+//
+//  CustomTabBarVC.swift
+//  CustomTabBar
+//
+//  Created by Hishara Dilshan on 2022-07-27.
+//
+
+import Foundation
+import UIKit
+
+class CustomTabBarVC: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
